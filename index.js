10cd5654f4d4d079818c345b4bf64a6c33ee99d0
mark <mddoyne@asu.edu>
2022-04-15 19:22:11 -0600

    save

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const gifts = [];
+
+function writeCards(gifts, )
+{
+    for(let i=0;i < gifts.length ;i++)
+    {
+        console.log(`Thankyou, ${gifts[i]}for the wonderful birthday gift`)
+    }
+}
